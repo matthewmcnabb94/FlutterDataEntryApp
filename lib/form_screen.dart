41f4354a34fa13c9cb31a9fbe341bb3380a187c7
Matthew McNabb <matthewoak@hotmail.com>
2020-09-01 13:25:14 +0100

    V 1.1

--- lib/form_screen.dart
+++ lib/form_screen.dart
@@ -3,10 +3,25 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:async';
 import 'package:flushbar/flushbar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 
 String id;
-List<String> _dropitems = ['A', 'B', 'C', 'D'];
-String _selectedType;
+String _customerName, customerNameNew;
+String _vehicleDetails, vehicleDetailsNew;
+String _turboModel, turboModelNew;
+String _price, priceNew;
+String _payment, paymentNew;
+String _fittingRequired, fittingRequiredNew;
+String _timeStamp;
+String c = ',';
+String a = "'";
+String fDate, _selectedDatePicked;
+bool datePicked = false;
+
+TimeOfDay selectedTime = TimeOfDay.now();
+
+DateTime selectedDate = DateTime.now();
 
 class FormScreen extends StatefulWidget {
   @override
@@ -16,19 +31,12 @@ class FormScreen extends StatefulWidget {
 }
 
 class FormScreenState extends State<FormScreen> {
-  String _name;
-  String _email;
-  String _password;
-  String _url;
-  String _phoneNumber;
-  String _calories;
-
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildName() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Name'),
-      maxLength: 10,
+      decoration: InputDecoration(labelText: 'Customer Name'),
+      maxLength: 25,
       validator: (String value) {
         if (value.isEmpty) {
           return 'Name is Required';
@@ -37,99 +45,108 @@ class FormScreenState extends State<FormScreen> {
         return null;
       },
       onSaved: (String value) {
-        _name = value;
+        _customerName = value;
       },
     );
   }
 
-  Widget _buildEmail() {
-    return TextFormField(
-      decoration: InputDecoration(labelText: 'Email'),
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Email is Required';
-        }
-
-        if (!RegExp(
-                r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-            .hasMatch(value)) {
-          return 'Please enter a valid email Address';
-        }
-
-        return null;
-      },
-      onSaved: (String value) {
-        _email = value;
-      },
-    );
+  Widget _buildDate() {
+    return RaisedButton(
+        child: Text(
+          'Select date order is due',
+          style: TextStyle(color: Colors.blue, fontSize: 16),
+        ),
+        onPressed: () {
+          _selectDate(context);
+        });
   }
 
-  Widget _buildPassword() {
+  Widget _buildVehicleDetails() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Password'),
+      decoration: InputDecoration(labelText: 'Vehilce Details'),
       keyboardType: TextInputType.visiblePassword,
       validator: (String value) {
         if (value.isEmpty) {
-          return 'Password is Required';
+          return 'vehicle details are Required';
         }
 
         return null;
       },
       onSaved: (String value) {
-        _password = value;
+        _vehicleDetails = value;
       },
     );
   }
 
-  Widget _builURL() {
+  Widget _builTurboModel() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Url'),
+      decoration: InputDecoration(labelText: 'Turbo Part Number'),
       keyboardType: TextInputType.url,
       validator: (String value) {
         if (value.isEmpty) {
-          return 'URL is Required';
+          return 'Turbo model is required';
         }
 
         return null;
       },
       onSaved: (String value) {
-        _url = value;
+        _turboModel = value;
       },
     );
   }
 
-  Widget _buildPhoneNumber() {
+  Widget _buildPrice() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Phone number'),
+      decoration: InputDecoration(labelText: 'Price'),
       keyboardType: TextInputType.phone,
       validator: (String value) {
         if (value.isEmpty) {
-          return 'Phone number is Required';
+          return 'Price is required';
         }
 
         return null;
       },
       onSaved: (String value) {
-        _url = value;
+        _price = value;
       },
     );
   }
 
-  Widget _buildCalories() {
+  Widget _buildPayment() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Calories'),
+      decoration: InputDecoration(labelText: 'Payment'),
       keyboardType: TextInputType.number,
       validator: (String value) {
-        int calories = int.tryParse(value);
+        //int calories = int.tryParse(value);
 
-        if (calories == null || calories <= 0) {
-          return 'Calories must be greater than 0';
+        //if (calories == null || calories <= 0) {
+        // return 'Calories must be greater than 0';
+        //}
+
+        if (value.isEmpty) {
+          return 'payment information is requried';
         }
 
         return null;
       },
       onSaved: (String value) {
-        _calories = value;
+        _payment = value;
+      },
+    );
+  }
+
+  Widget _buildFittingRequired() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Fitting Required'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Fitting required needed';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        _fittingRequired = value;
       },
     );
   }
@@ -153,48 +170,56 @@ class FormScreenState extends State<FormScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Form Demo")),
-      body: Container(
-        margin: EdgeInsets.all(24),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _buildName(),
-              _buildEmail(),
-              _buildPassword(),
-              _builURL(),
-              _buildPhoneNumber(),
-              _buildCalories(),
-              // _buildDropdown(),
-              SizedBox(height: 100),
-              RaisedButton(
-                child: Text(
-                  'Submit',
-                  style: TextStyle(color: Colors.blue, fontSize: 16),
-                ),
-                onPressed: () {
-                  if (!_formKey.currentState.validate()) {
-                    return;
-                  }
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildName(),
+                _buildDate(),
+                _buildVehicleDetails(),
+                _builTurboModel(),
+                _buildPrice(),
+                _buildPayment(),
+                _buildFittingRequired(),
+                // _buildDropdown(),
+                SizedBox(height: 100),
+                RaisedButton(
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(color: Colors.blue, fontSize: 16),
+                  ),
+                  onPressed: () {
+                    if (datePicked) {
+                      if (!_formKey.currentState.validate()) {
+                        return;
+                      }
 
-                  _formKey.currentState.save();
+                      _formKey.currentState.save();
 
-                  print(_name);
-                  print(_email);
-                  print(_phoneNumber);
-                  print(_url);
-                  print(_password);
-                  print(_calories);
+                      print(_customerName);
 
-                  print("Data sent");
+                      //turboModelNew =
+                      //_turboModel.replaceAll(new RegExp(r"\s+"), "");
 
-                  checkIfSuccess(context);
+                      //caloriesNew = _calories.replaceAll(new RegExp(r"\s+"), "");
 
-                  //Send to API
-                },
-              )
-            ],
+                      print("Data sent");
+
+                      checkIfSuccess(context);
+                    } else {
+                      print('date not picked yet');
+                      showSimpleFlushbarDateNotPicked(context);
+                    }
+
+                    //Send to API
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -217,6 +242,12 @@ checkIfSuccess(BuildContext context) async {
 Future<bool> sendData() async {
   bool success;
   Socket socket;
+
+  var now = new DateTime.now();
+  var formatter = new DateFormat('dd-MM-yyyy HH:mm:ss');
+  _timeStamp = formatter.format(now);
+  print(_timeStamp); // 2016-01-25
+
   try {
     socket =
         await Socket.connect('77.68.122.181', 80).timeout(Duration(seconds: 5));
@@ -227,15 +258,21 @@ Future<bool> sendData() async {
       //print(utf8.decode(event));
       print('received from JAVA server is: ' + utf8.decode(event));
       id = utf8.decode(event);
+      print('ID is: ' + id);
       if (id.length > 0) {
         success = true;
+        print('reply received from server');
       } else {
         success = false;
       }
     });
 
     // send hello
-    socket.add(utf8.encode('CREATE,' + 'testdata' + '\n'));
+    socket.add(utf8.encode(
+        'CREATE,($a$_customerName$a $c $a$fDate$a $c $a$_vehicleDetails$a $c $a$_turboModel$a $c $a$_price$a $c $a$_payment$a' +
+            '$c $a$_fittingRequired$a $c $a$_timeStamp$a' +
+            ')' +
+            '\n'));
 
     print('sending to server');
 
@@ -254,7 +291,7 @@ Future<bool> sendData() async {
 
 void showSimpleFlushbarSuccess(BuildContext context) {
   Flushbar(
-    message: 'Your entry was a success!!',
+    message: id,
     mainButton: FlatButton(
       child: Text(
         'ok',
@@ -278,4 +315,35 @@ void showSimpleFlushbarError(BuildContext context) {
     duration: Duration(seconds: 15),
     backgroundColor: Colors.red,
   )..show(context);
+}
+
+void showSimpleFlushbarDateNotPicked(BuildContext context) {
+  Flushbar(
+    message: 'You have not selected order due date',
+    mainButton: FlatButton(
+      child: Text(
+        'ok',
+      ),
+      onPressed: () {},
+    ),
+    duration: Duration(seconds: 3),
+    backgroundColor: Colors.red,
+  )..show(context);
+}
+
+Future<void> _selectDate(BuildContext context) async {
+  final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101));
+
+  if (picked != null && picked != selectedDate) {
+    print(picked.toString());
+    datePicked = true;
+    var now1 = new DateTime.now();
+    var formatter1 = new DateFormat('dd-MM-yyyy');
+
+    fDate = formatter1.format(picked);
+  }
 }
